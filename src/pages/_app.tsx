70417ddfc94c05c119ef5be5bbc4b897abbea0db
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-11-23 14:39:51 +0800

    Add Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import { Fragment } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+import { MantineProvider } from "@mantine/core";
+
 import "normalize.css";
+import "@mantine/core/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content={"width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0"} />
       </Head>
       
-      <Component {...pageProps} />
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
     </Fragment>
   );
 };
