3db2e491afc794b5f50267595d37ecb9c507fa68
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-12-06 19:18:53 +0800

    Redefine eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,14 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
-
-export default defineConfig([{
-  extends: compat.extends("next/core-web-vitals", "next/typescript"),
-}]);
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTs,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ])
+]);
